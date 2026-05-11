e54e206450f6878cd16a66f2ee1a2a294c205a0f
mugisha <mfabricerwath@gmail.com>
2026-05-11 09:43:44 +0200

    component signalEx added

---
+++ src/app/components/signal-ex/signal-ex.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-ex',
+  imports: [],
+  templateUrl: './signal-ex.html',
+  styleUrl: './signal-ex.css',
+})
+export class SignalEx {}
