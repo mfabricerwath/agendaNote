e45e85c0e12de643c50130ec80cd558c467804c5
mugisha <mfabricerwath@gmail.com>
2026-05-12 15:21:21 +0200

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
+import { User } from './components/user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:Admin
+    },
+    {
+        path:'admin',
+        component:Admin
+    },
+    {
+        path:'control-flow-statement',
+        component:ControlFlow
+    },
+    {
+        path:'data-binding',
+        component:DataBinding
+    },
+    {
+        path:'signal-ex',
+        component:SignalEx
+    },
+    {
+        path:'user',
+        component:User
+    },
+];
