b4e72a8fd572014b365a6966a22f845a4645fb96
mugisha <mfabricerwath@gmail.com>
2026-05-16 14:12:57 +0200

    lifecycle,event and pipe

--- src/app/components/att-directive/att-directive.ts
+++ src/app/components/att-directive/att-directive.ts
@@ -1,21 +1,80 @@
-import { NgClass, NgStyle } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  NgClass,
+  NgStyle,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+  JsonPipe,
+  DatePipe,
+} from '@angular/common';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass, NgStyle],
+  imports: [NgClass, NgStyle, UpperCasePipe, TitleCasePipe, SlicePipe, JsonPipe, DatePipe],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css',
 })
-export class AttDirective {
-  bgClassName=signal<string>("");
-  isDiv2Green: boolean=false;
+export class AttDirective
+  implements
+    OnInit,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy
+{
+  firstName: string = 'Fabrice';
+  courseName: string = 'Angular Developpement course';
+  bgClassName = signal<string>('');
+  isDiv2Green: boolean = false;
 
-  setbgclass(className: string){
+  NumbersList: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  studentsList: { id: number; name: string }[] = [
+    { id: 1, name: 'Fabrice' },
+    { id: 2, name: 'Alice' },
+    { id: 3, name: 'Bob' },
+    { id: 4, name: 'Charlie' },
+    { id: 5, name: 'David' },
+  ];
+  currentDate: Date = new Date();
+
+  setbgclass(className: string) {
     this.bgClassName.set(className);
   }
 
-  toggleDiv2Color(){
-    this.isDiv2Green=!this.isDiv2Green;
+  toggleDiv2Color() {
+    this.isDiv2Green = !this.isDiv2Green;
+  }
+
+  ngOnInit(): void {
+    console.log('AttDirective ngOnInit called');
+  }
+  ngAfterContentInit(): void {
+    console.log('AttDirective ngAfterContentInit called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('AttDirective ngAfterViewChecked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('AttDirective ngAfterViewInit called');
+  }
+  ngOnDestroy(): void {
+    console.log('AttDirective ngOnDestroy called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('AttDirective ngAfterContentChecked called');
+  }
+  constructor() {
+    console.log('AttDirective constructor called');
   }
 }
---
+++ src/app/components/get-api/get-api.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  imports: [],
+  templateUrl: './get-api.html',
+  styleUrl: './get-api.css',
+})
+export class GetApi {}
